f98d9ab9c7fff8fe094efe7be81af38336e635fd
James Pederson <james@jpederson.com>
2024-04-30 23:34:36 +0000

    Add favicon to head code.

--- header.php
+++ header.php
@@ -18,6 +18,14 @@
 <?php wp_head(); ?>
 <link href="<?php bloginfo( "template_url" ) ?>/css/main.css" rel="stylesheet" type="text/css">
 
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#813996">
+<meta name="msapplication-TileColor" content="#813996">
+<meta name="theme-color" content="#ffffff">
+
 </head>
 <body <?php body_class(); ?>>
 <div class="container">
